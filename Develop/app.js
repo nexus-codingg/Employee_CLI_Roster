d87409245b44399ccc8483bfbbcd53f902666ab9
Keiana Hanson <kiki_hanson@aol.com>
2020-07-18 23:34:14 -0400

    added Manger, Intern, Engineer, Common Questions inquier prompts, testing CLI now

--- Develop/app.js
+++ Develop/app.js
@@ -10,28 +10,163 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+//Array to store employees as they are created
+const team = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+//Welcome message and prompt initiation
+function startTeam() {
+    console.log('Welcome to your CLI Employee Summary Generator.');
+    selectEmployee();
+};
 
+//First ask the user which type of employee they will be creating
+const teamPrompt = {
+    type: 'list',
+    name: 'newEmployee',
+    message: 'Which type of employee would you like to add to your team?',
+    choices: ['Manager', 'Engineer', 'Intern'],
+};
 
+//Based on the user's answer to the employeePrompt, ask the set of questions associated
+//with the type of employee they will be adding
+function selectEmployee() {
+    inquirer.prompt(teamPrompt).then((answers) => {
+        if (answers.newEmployee === 'Manager') {
+            console.log("Great! Let's create a Manager for your team.");
+            newManager();
+        } else if (answers.newEmployee === 'Engineer') {
+            console.log("Great! Let's create an Engineer for your team.");
+            newEngineer();
+        } else {
+            console.log("Great! Let's create an Intern for your team.");
+            newIntern();
+        }
+    });
+};
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+//Array containing questions common to all employees and validates information entered is correct 
+const commonInputs = ([
+    {
+        type: 'input',
+        name: 'employeeName',
+        message: 'Please enter the name for the employee you are adding to your team.',
+        validate: function (value) {
+            let pass = value.match(
+                // using regex, makes sure name entered only consists of letters a-z or A-Z
+                /^[a-zA-Z]+$/
+            );
+            if (pass) {
+                return true;
+            }
+            return 'Please enter a valid name, using *only* letters a-z or A-Z.'
+        },
+    },
+    {
+        type: 'input',
+        name: 'employeeID',
+        message: 'Please enter the ID for the employee you are adding to your team.',
+        validate: function (value) {
+            // to make susre the information entered is a number, then returns only the first number of the string
+            var valid = !isNaN(parseFloat(value));
+            return valid || 'Please enter a number';
+          },
+    },
+    {
+        type: 'input',
+        name: 'employeeEmail',
+        message: 'Please enter the email for the employee you are adding to your team.',
+        validate: function (value) {
+            let pass = value.match(
+                // using regex, makes sure email entered is in a valid format with a 2 or 3 letter domain, such as test@test.com, or test@test.uk
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+            );
+            if (pass) {
+                return true;
+            }
+            return 'Please enter a valid email address; example format: test@test.com; test@test.uk'
+        },
+    }
+]);
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+//If the user selects "Manager" is selected from the teamPrompt, the user is presented with these questions.
+const managerPrompts = () => {
+    const managerPrompt = {
+        type: 'input',
+        name: 'managerNumber',
+        message: 'Please enter the office number for the manager you are adding to your team.',
+    };
+    // attaches the answers from our commonInputs() to our manager answers and returns all the information
+    const managerInfo = commonInputs.concat(managerPrompt);
+    return managerInfo;
+};
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+//Generates new Manager instance based on user input and pushes to team array
+function newManager() {
+    inquirer.prompt(managerPrompts()).then((answers) => {
+        const newManager = new Manager(answers.employeeName, answers.employeeID, answers.employeeEmail, answers.managerNumber);
+        team.push(newManager);
+        console.log(newManager);
+    }).then(addMorePrompt);
+};
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+//When Engineer is selected from the employeePrompt, the user is presented with these questions.
+const engineerPrompts = () => {
+    const engineerPrompt = {
+        type: 'input',
+        name: 'engineerGitHub',
+        message: 'Please enter the GitHub username for the engineer you are adding to your team.',
+    };
+    // attaches the answers from our commonInputs() to our engineer answers and returns all the information
+    const engineerInfo = commonInputs.concat(engineerPrompt);
+    return engineerInfo;
+};
+
+//Generate a new Engineer instance based on user input and push to employee array
+function newEngineer() {
+    inquirer.prompt(engineerPrompts()).then((answers) => {
+        const newEngineer = new Engineer(answers.employeeName, answers.employeeID, answers.employeeEmail, answers.engineerGitHub);
+        team.push(newEngineer);
+        console.log(newEngineer);
+    }).then(addMorePrompt);
+};
+
+//When Intern is selected from the employeePrompt, the user is presented with these questions.
+const internPrompts= () => {
+    const internPrompt = {
+        type: 'input',
+        name: 'internSchool',
+        message: 'Please enter the school name for the intern you are adding to your team.',
+    };
+    // attaches the answers from our commonInputs() to our intern answers and returns all the information
+    const internInfo = commonInputs.concat(internPrompt);
+    return internInfo;
+};
+//Generate a new Intern instance based on user input and push to employee array
+function newIntern() {
+    inquirer.prompt(internPrompts()).then((answers) => {
+        const newIntern = new Intern(answers.employeeName, answers.employeeID, answers.employeeEmail, answers.internSchool);
+        team.push(newIntern);
+        console.log(newIntern);
+    }).then(addMorePrompt);
+};
+
+//Asks the user if they want to add another employee after each entry
+function addMorePrompt() {
+    inquirer.prompt({
+        type: 'confirm',
+        name: 'addAnotherEmployee',
+        message: 'Would you like to add another employee?'
+    }).then(answers => {
+        if (answers.addAnotherEmployee) {
+            selectEmployee();
+        } else {
+            console.log('Thanks for using CLI Employee Summary Generator! Your team page has been created.');
+            //When the user has finished entering all team members, call a function to render
+            //the HTML and generate the team page
+            outputTeamHTML(team);
+
+        };
+    });
+};
+
+startTeam();
